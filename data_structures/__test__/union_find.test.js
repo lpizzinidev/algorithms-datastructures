f9478291d251eb6902ba4dca61c9166f25dea8b3
Luca Pizzini <lpizzini7@gmail.com>
2022-03-11 16:12:16 +0100

    [UnionFind] Added implementation

---
+++ data_structures/__test__/union_find.test.js
@@ -0,0 +1,50 @@
+const { UnionFind } = require('../union_find');
+
+describe('UnionFind', () => {
+  test('constructor', () => {
+    const unionFind = new UnionFind(10);
+    unionFind.union(1, 2);
+    unionFind.union(2, 5);
+    unionFind.union(5, 6);
+    unionFind.union(6, 7);
+    unionFind.union(3, 8);
+    unionFind.union(8, 9);
+
+    expect(unionFind.connected(1, 5)).toBe(true);
+    expect(unionFind.connected(5, 7)).toBe(true);
+    expect(unionFind.connected(4, 9)).toBe(false);
+
+    unionFind.union(9, 4);
+
+    expect(unionFind.connected(4, 9)).toBe(true);
+  });
+
+  test('set with multiple components', () => {
+    const unionFind = new UnionFind(5);
+    unionFind.union(0, 1);
+    unionFind.union(1, 2);
+    unionFind.union(3, 4);
+
+    expect(unionFind.connectedComponents).toBe(2);
+  });
+
+  test('adding the same edge should not change the result', () => {
+    const unionFind = new UnionFind(5);
+    unionFind.union(0, 1);
+    unionFind.union(1, 2);
+    unionFind.union(3, 4);
+    unionFind.union(4, 3);
+
+    expect(unionFind.connectedComponents).toBe(2);
+  });
+
+  test('set with a single components', () => {
+    const unionFind = new UnionFind(5);
+    unionFind.union(0, 1);
+    unionFind.union(1, 2);
+    unionFind.union(2, 3);
+    unionFind.union(3, 4);
+
+    expect(unionFind.connectedComponents).toBe(1);
+  });
+});
---
+++ data_structures/union_find.js
@@ -0,0 +1,47 @@
+/**
+ * Union find data structure optimized with path compression
+ * and union by rank
+ */
+class UnionFind {
+  constructor(size) {
+    this.size = size;
+
+    this.root = Array(size)
+      .fill(0)
+      .map((v, idx) => idx);
+    // Use a rank array to record the height of each vertex, the "rank" of each vertex
+    this.rank = Array(size).fill(1);
+    // Keep track of the number of connected components in the graph
+    this.connectedComponents = size;
+  }
+
+  // Optimize the function by applying path compression recursively
+  find(x) {
+    if (x === this.root[x]) return x;
+    this.root[x] = this.find(this.root[x]);
+    return this.root[x];
+  }
+
+  // The union function is optimized with union by rank
+  union(x, y) {
+    const rootX = this.find(x);
+    const rootY = this.find(y);
+    if (rootX !== rootY) {
+      if (this.rank[rootX] > this.rank[rootY]) {
+        this.root[rootY] = rootX;
+      } else if (this.rank[rootX] < this.rank[rootY]) {
+        this.root[rootX] = rootY;
+      } else {
+        this.root[rootY] = rootX;
+        this.rank[rootX]++;
+      }
+      this.connectedComponents--;
+    }
+  }
+
+  connected(x, y) {
+    return this.find(x) === this.find(y);
+  }
+}
+
+module.exports = { UnionFind };
