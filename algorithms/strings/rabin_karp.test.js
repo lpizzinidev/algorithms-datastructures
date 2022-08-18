8ee6e391ae8cd88cee031aed24e0eb4b249f1cfb
Luca Pizzini <lpizzini7@gmail.com>
2022-08-18 20:24:57 +0200

    added test cases

--- algorithms/strings/rabin_karp.test.js
+++ algorithms/strings/rabin_karp.test.js
@@ -9,6 +9,22 @@ describe('Rabin-Karp', () => {
     expect(res).toEqual(expected);
   });
 
+  test('Should find matches in a string with special characters', () => {
+    const txt = 'P@TTerNabcdefP@TTerNP@TTerNabcdefabcdefabcdefabcdefP@TTerN';
+    const pat = 'P@TTerN';
+    const expected = [0, 13, 20, 51];
+    const res = rabinKarp(txt, pat);
+    expect(res).toEqual(expected);
+  });
+
+  test('Should find matches in a string when pattern is a single character', () => {
+    const txt = 'ababababa';
+    const pat = 'a';
+    const expected = [0, 2, 4, 6, 8];
+    const res = rabinKarp(txt, pat);
+    expect(res).toEqual(expected);
+  });
+
   test('Should return an empty array if strings does not match', () => {
     const txt = 'AABAACAADAABAABA';
     const pat = 'AABAX';
