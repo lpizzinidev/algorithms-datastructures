dcd2517e9c49ad9a269ca593faf10070bccec7f9
Luca Pizzini <lpizzini7@gmail.com>
2022-09-15 20:23:50 +0200

    added comments

--- data_structures/b_tree/b_tree.js
+++ data_structures/b_tree/b_tree.js
@@ -15,42 +15,56 @@ class BTree {
     this.t = t;
   }
 
+  // Searches a key in the tree, return the node if present
   search(k) {
     if (this.root === null) return null;
     return this._search(k, this.root);
   }
 
+  // Recusively search a key in the subtree rooted at x
   _search(k, x) {
     let idx = 0;
+    // Traverse the root nodes of x until a value greater or
+    // equal to k is found
     while (idx < x.n && k > x.keys[idx]) idx++;
+    // Key found, return the node
     if (x.keys[idx] === k) return x;
+    // The node is a leaf, return null
     if (x.leaf) return null;
+    // Iterate on the idx-th child
     return this._search(k, x.children[idx]);
   }
 
   // Insert a new value in the tree
   insert(k) {
     if (this.root === null) {
+      // Empty tree, add a new root node
       this.root = new BTreeNode(this.t, true);
       this.root.keys[0] = k;
       this.root.n = 1;
     } else {
       let r = this.root;
+      // If the root level is full
       if (r.n === 2 * this.t - 1) {
+        // Split the root and add a new node as root
         const s = new BTreeNode(this.t, false);
         s.children[0] = this.root;
         this.split(s, 0, this.root);
-        this.insertValue(s, k);
+        let idx = 0;
+        if (s.keys[0] < k) idx++;
+        this.insertNonFull(s.children[idx], k);
         this.root = s;
-      } else {
-        this.insertValue(r, k);
       }
+      // If the root level has space, add the node here
+      else this.insertNonFull(r, k);
     }
   }
 
-  insertValue(x, k) {
+  // Inserts a key in the non full root list of x
+  insertNonFull(x, k) {
     let idx = x.n - 1;
     if (x.leaf) {
+      // x is a leaf node, search the right position where to insert the new key
       while (idx >= 0 && k < x.keys[idx]) {
         x.keys[idx + 1] = x.keys[idx];
         idx--;
@@ -58,41 +72,44 @@ class BTree {
       x.keys[idx + 1] = k;
       x.n++;
     } else {
+      // x is not a leaf, determine on what child to recur
       while (idx >= 0 && k < x.keys[idx]) {
         idx--;
       }
       idx++;
       const temp = x.children[idx];
-      if (temp.n === 2 * t - 1) {
+      // If the child is full, split it
+      if (temp.n === 2 * this.t - 1) {
         this.split(x, idx, temp);
         if (k > x.keys[idx]) idx++;
       }
-      this.insertValue(x.children[idx], k);
+      // Recurse on the child
+      this.insertNonFull(x.children[idx], k);
     }
   }
 
   // Split the i-th full children of a node x
   split(x, i, y) {
-    const z = new BTreeNode(y.t, y.leaf);
+    const z = new BTreeNode(this.t, y.leaf);
     z.n = this.t - 1;
     for (let j = 0; j < this.t - 1; j++) {
-      z.keys[j] = x.keys[j + this.t];
+      z.keys[j] = y.keys[j + this.t];
     }
-    if (!x.leaf) {
+    if (!y.leaf) {
       for (let j = 0; j < this.t; j++) {
         z.children[j] = y.children[j + this.t];
       }
     }
-    x.n = this.t - 1;
-    for (let j = n; j >= i + 1; j--) {
-      this.children[j + 1] = this.children[j];
+    y.n = this.t - 1;
+    for (let j = x.n; j >= i + 1; j--) {
+      x.children[j + 1] = x.children[j];
     }
-    this.children[i + 1] = z;
-    for (let j = n - 1; j >= i; j--) {
-      this.keys[j + 1] = this.keys[j];
+    x.children[i + 1] = z;
+    for (let j = x.n - 1; j >= i; j--) {
+      x.keys[j + 1] = x.keys[j];
     }
-    this.keys[i] = x.keys[this.t - 1];
-    this.n++;
+    x.keys[i] = y.keys[this.t - 1];
+    x.n++;
   }
 }
 
--- data_structures/b_tree/b_tree.test.js
+++ data_structures/b_tree/b_tree.test.js
@@ -9,9 +9,13 @@ describe('B-tree', () => {
     tree.insert(9);
     tree.insert(12);
     tree.insert(24);
+    tree.insert(34);
+    tree.insert(59);
+    tree.insert(101);
+    tree.insert(139);
 
-    expect(tree.search(12)).not.tobe(null);
-    expect(tree.search(24)).not.tobe(null);
-    expect(tree.search(30)).tobe(null);
+    expect(tree.search(12)).not.toBeNull();
+    expect(tree.search(24)).not.toBeNull();
+    expect(tree.search(30)).toBeNull();
   });
 });
