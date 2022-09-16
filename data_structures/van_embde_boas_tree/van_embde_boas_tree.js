9c6d62429ea74e93bbc6b87b55231b1f0f996921
Luca Pizzini <lpizzini7@gmail.com>
2022-09-16 21:58:57 +0200

    added base van Emde Boas tree operations

--- data_structures/van_embde_boas_tree/van_embde_boas_tree.js
+++ data_structures/van_embde_boas_tree/van_embde_boas_tree.js
@@ -10,11 +10,11 @@ class VanEmdeBoasTree {
     this.max = null;
     if (size <= 2) {
       this.summary = null;
-      this.clusters = [];
+      this.cluster = [];
     } else {
       const numClusters = Math.ceil(Math.sqrt(size));
       this.summary = new VanEmdeBoasTree(numClusters);
-      this.clusters = new Array(numClusters)
+      this.cluster = new Array(numClusters)
         .fill()
         .map(() => new VanEmdeBoasTree(numClusters));
     }
@@ -37,12 +37,49 @@ class VanEmdeBoasTree {
   }
 
   member(x) {
-    return this._member(this, x);
+    if (x === this.min || x === this.max) return true;
+    if (this.u === 2) return false;
+    return this.cluster[this.high(x)].member(this.low(x));
   }
 
-  _member(tree, x) {
-    if (x === tree.min || x === tree.max) return true;
-    if (tree.u === 2) return false;
-    return this._member(tree.clusters[this.high(x)], this.low(x));
+  index(x, y) {
+    return x * Math.ceil(math.sqrt(this.u)) + y;
+  }
+
+  predecessor(x) {
+    if (this.u === 2) {
+      if (x === 1 && this.min === 0) return 0;
+      return null;
+    }
+    if (this.max !== null && x > this.max) return this.max;
+    const minInCluster = this.cluster[this.high(x)].minimum();
+    if (minInCluster !== null && this.low(x) > minInCluster) {
+      const offset = this.cluster[this.high(x)].predecessor(this.low(x));
+      return this.index(this.high(x), offset);
+    }
+    const predCluster = this.summary.predecessor(this.high(x));
+    if (predCluster === null) {
+      if (this.min !== null && x > this.min) return this.minimum;
+      return null;
+    }
+    const offset = this.cluster[predCluster].maximum();
+    return this.index(predCluster, offset);
+  }
+
+  successor(x) {
+    if (this.u === 2) {
+      if (x === 0 && this.max === 1) return 1;
+      return null;
+    }
+    if (this.min !== null && x < this.min) return this.min;
+    const maxInCluster = this.cluster[this.high(x)].maximum();
+    if (maxInCluster !== null && this.low(x) < maxInCluster) {
+      const offset = this.cluster[this.high(x)].successor(this.low(x));
+      return this.index(this.high(x), offset);
+    }
+    const succCluster = this.summary.successor(this.high(x));
+    if (succCluster === null) return null;
+    const offset = this.cluster[succCluster].minimum();
+    return this.index(succCluster, offset);
   }
 }
